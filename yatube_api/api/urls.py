53e223b7e492264eaf5c64ff0d134384e47d648d
Evisst <maksmaks2000.com@gmail.com>
2025-05-03 23:01:22 +0300

    fix

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,21 +6,16 @@ from .views import PostViewSet, GroupViewSet, CommentViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet,
-                basename='posts')
-router.register(r'groups', GroupViewSet,
-                basename='groups')
-
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'groups', GroupViewSet, basename='groups')
 
 posts_router = nested_routers.NestedDefaultRouter(
     router, r'posts', lookup='post'
 )
-posts_router.register(r'comments', CommentViewSet,
-                      basename='post-comments')
-
+posts_router.register(r'comments', CommentViewSet, basename='post-comments')
 
 urlpatterns = [
-        path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
-        path('', include(router.urls)),
-        path('', include(posts_router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
+    path('', include(router.urls)),
+    path('', include(posts_router.urls)),
 ]
